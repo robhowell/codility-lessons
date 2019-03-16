6007cf9d54102d36cdb8fc6f406f40ceadd2b85a
Rob Howell <rob@robhowell.com>
2019-03-16 21:22:48 +0000

    Working solution for binary gap

--- lesson-1-iterations/binary-gap/index.js
+++ lesson-1-iterations/binary-gap/index.js
@@ -1,4 +1,31 @@
-export default function solution(N) {
-  console.log('test');
+const findGap = (binaryArray, gaps) => {
+  const firstOne = binaryArray.indexOf('1');
+
+  if (firstOne !== -1) {
+    const binaryArrayTrimmed = binaryArray.slice(firstOne + 1);
+
+    const secondOne = binaryArrayTrimmed.indexOf('1');
+
+    if (secondOne > 0) {
+      gaps.push(secondOne);
+    }
+
+    return findGap(binaryArrayTrimmed.slice(secondOne + 1), gaps);
+  }
+
+  if (gaps.length) return Math.max(...gaps);
+
   return 0;
+};
+
+export default function solution(N) {
+  if (N !== parseInt(N, 10)) return 0; // Early return if not a base-10 int
+  if (N < 1 || N > 2147483647) return 0; // Early return if not in range
+
+  const binaryArray = N.toString(2).split('');
+  const numberOfGaps = findGap(binaryArray, []);
+
+  return numberOfGaps;
 }
+
+console.log(solution(17));
--- lesson-1-iterations/binary-gap/index.spec.js
+++ lesson-1-iterations/binary-gap/index.spec.js
@@ -1,7 +1,18 @@
 import solution from './index';
 
 describe('binary gap solution', () => {
-  test('17: 10001', () => {
-    expect(solution(17)).toBe(5);
+  it('standard test cases', () => {
+    expect(solution(1041)).toBe(5); // 10000010001
+    expect(solution(15)).toBe(0); // 1111
+    expect(solution(32)).toBe(0); // 100000
+  });
+
+  it('custom test cases', () => {
+    expect(solution(9)).toBe(2); // 1001
+    expect(solution(17)).toBe(3); // 10001
+    expect(solution(1007)).toBe(1); // 1111101111
+    expect(solution(1023)).toBe(0); // 1111111111
+    expect(solution(8737)).toBe(4); // 10001000100001
+    expect(solution(4368)).toBe(3); // 1000100010000
   });
 });
