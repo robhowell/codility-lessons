4bb04f7c4564d88b2074aee8691a92b65157e2ec
Rob Howell <rob@robhowell.com>
2019-03-17 13:15:51 +0000

    Improved solution for odd-occurrences that passes all performance tests

--- lesson-2-arrays/odd-occurrences-in-arrays/index.js
+++ lesson-2-arrays/odd-occurrences-in-arrays/index.js
@@ -1,30 +1,17 @@
-/* globals BigInt */
-
 export default function solution(A) {
-  const found = [];
+  const found = new Map();
 
-  for (let index = 0; index < A.length; index++) {
-    const num = A[index];
+  for (const num of A) {
+    const count = found.get(num);
 
-    if (typeof found[num] === 'undefined') {
-      found[num] = 1;
+    if (count) {
+      found.set(num, count + 1);
     } else {
-      found[num]++;
+      found.set(num, 1);
     }
   }
 
-  for (let index = 0; index < found.length; index++) {
-    const repeats = found[index];
-
-    if (repeats % 2 === 1) return index;
+  for (const [num, count] of found) {
+    if (count % 2 === 1) return num;
   }
 }
-
-const start = process.hrtime.bigint();
-for (let index = 0; index < 10000; index++) {
-  solution([9, 3, 9, 3, 9, 7, 9, 7, 4]);
-}
-const end = process.hrtime.bigint();
-const nanoseconds = end - start;
-const microseconds = nanoseconds / BigInt(1000);
-console.log(`Benchmark took ${microseconds} microseconds`);
