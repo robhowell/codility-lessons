51badb9e959784428c9b2a55451f6030415086d5
Rob Howell <rob@robhowell.com>
2019-03-17 12:53:41 +0000

    Add working solution for odd-occurrences-in-arrays except for performance

--- lesson-2-arrays/odd-occurrences-in-arrays/index.js
+++ lesson-2-arrays/odd-occurrences-in-arrays/index.js
@@ -1,3 +1,30 @@
-export default function solution(A) {}
+/* globals BigInt */
 
-solution([9, 3, 9, 3, 9, 7, 9]);
+export default function solution(A) {
+  const found = [];
+
+  for (let index = 0; index < A.length; index++) {
+    const num = A[index];
+
+    if (typeof found[num] === 'undefined') {
+      found[num] = 1;
+    } else {
+      found[num]++;
+    }
+  }
+
+  for (let index = 0; index < found.length; index++) {
+    const repeats = found[index];
+
+    if (repeats % 2 === 1) return index;
+  }
+}
+
+const start = process.hrtime.bigint();
+for (let index = 0; index < 10000; index++) {
+  solution([9, 3, 9, 3, 9, 7, 9, 7, 4]);
+}
+const end = process.hrtime.bigint();
+const nanoseconds = end - start;
+const microseconds = nanoseconds / BigInt(1000);
+console.log(`Benchmark took ${microseconds} microseconds`);
--- lesson-2-arrays/odd-occurrences-in-arrays/index.spec.js
+++ lesson-2-arrays/odd-occurrences-in-arrays/index.spec.js
@@ -1,9 +1,12 @@
 import solution from './index';
 
 describe('odd occurrences in array solution', () => {
-  // it('standard test cases', () => {});
+  it('standard test cases', () => {
+    expect(solution([9, 3, 9, 3, 9, 7, 9])).toEqual(7);
+  });
 
   it('custom test cases', () => {
-    expect(solution([9, 3, 9, 3, 9, 7, 9])).toEqual(7);
+    expect(solution([9, 3, 9, 3, 9, 7, 9, 7, 4])).toEqual(4);
+    expect(solution([0, 9, 3, 9, 3, 9, 7, 9, 7])).toEqual(0);
   });
 });
